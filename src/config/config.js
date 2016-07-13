fd7af8ca68e841c9a2ee6eb4dd47297baa4f7084
Oliver Friedmann <oliver.friedmann@gmail.com>
2016-07-12 22:50:56 -0400

    Gitignore; Cleanup; Add Attrs in Components

--- src/config/config.js
+++ src/config/config.js
@@ -2,15 +2,3 @@
 $(document).ready(function () {
     BetaJS.Dynamics.Dynamic.activate({element: document.body});
 });
-
-//var router = new BetaJS.Router.Router();
-//
-//window.appstate = new BetaJS.Properties.Properties({
-//    component_type: components[0]
-//});
-//
-//router.bind("components", "/components/(component:.+)", function (args) {
-//    window.appstate.set("component_type", args.component);
-//});
-//
-//router.navigate(document.location.hash.substring(1));
